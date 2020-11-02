bc5c22f394d1e3e40364bcc1985953f04af12bbd
nicksandoval <sand8val@gmail.com>
2020-11-02 14:16:32 -0800

    validation error message added.

--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,10 @@ const transactionType = document.getElementById('transaction-type')
 const transactionAmount = document.getElementById('transaction-amount')
 
 transactionBtn.addEventListener('click', (event) => {
-  
-    if (!transactionType.value || !transactionName.value || transactionAmount.value) {
-        console.log('please enter value')
+    if (transactionType.value == 'Type' || !transactionName.value || !transactionAmount.value) {
+        document.getElementById('error-message').innerHTML = 'All fields required.'
     } else {
+        document.getElementById('error-message').innerHTML = ''
         console.log(event.target)
     }
     
